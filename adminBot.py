faf76d3d0d20949cff07df87b1261fd14b468467
Kaan Sarıkaya <kadirkaansarikaya@gmail.com>
2020-07-10 18:13:59 +0300

    mute and unmute

--- adminBot.py
+++ adminBot.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import requests
 import os
+from discord.ext.commands import has_permissions, MissingPermissions
+from discord import Member
 
 payload = {}
 
@@ -10,13 +12,37 @@ client = commands.Bot(command_prefix = ';;')
 @client.event
 async def on_ready():
     activity = discord.Game(name=";;help", type=3)
-    await client.change_presence(status=discord.Status.idle, activity=activity)
+    await client.change_presence(status=discord.Status.online, activity=activity)
     print('Bot is ready')
 
 
 @client.command()
-async def help(ctx, *, link):
+async def hey(ctx):
     
-    await ctx.send(f';;help  -> help for use bot')
+    await ctx.send(f'hop')
 
-client.run('***********************') #BOT AUTH TOKEN
+@client.command()
+async def mute(ctx, member: Member):
+    guild = ctx.guild
+    if(str(discord.utils.get(ctx.guild.roles, name="muted")) == "None"):
+        await guild.create_role(name="muted")
+        await ctx.channel.set_permissions(discord.utils.get(ctx.guild.roles, name="muted"), send_messages=False)
+
+    
+    role = discord.utils.get(ctx.guild.roles, name="muted")
+    await member.add_roles(role)
+    
+    await(await ctx.send(f'muted')).delete(delay=3)
+
+@client.command()
+async def unmute(ctx, member: Member):
+    guild = ctx.guild
+  
+    role = discord.utils.get(ctx.guild.roles, name="muted")
+    
+    await ctx.channel.set_permissions(role, send_messages=False)
+    await member.remove_roles(role)
+    
+    await(await ctx.send(f'unmuted')).delete(delay=3)
+
+client.run('NzMxMDk1MTc3MzAwNTQxNDcx.XwhEnQ.KeebZQwFTysBG2Ae0V1RieJt3q8') #BOT AUTH TOKEN
